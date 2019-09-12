e5645f097df4bf9d2b332087c70c424525073902
h64 <hhongnr@gmail.com>
2019-09-12 11:13:17 -0700

    added login route

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,31 @@ const router = require('express').Router();
 const db = require('../models');
 
 router.post('/login', (req, res) => {
-    res.send('STUB - post /auth/login')
+    // Fint eh user by their email in the db
+    db.User.findOne({ email: req.body.email })
+    .then(user => {
+        // make sure we have a user, and the user has a password
+        if(!user || !user.password) {
+            return res.status(404).send({ message: 'User not found' })
+        }
+
+        // yay - a user. Let's check their password
+        if(!user.isAuthenticated(req.body.password)) {
+            // invalid credentials - wrong password
+            return res.status(406).send({ messsage: 'Not Acceptable: Invalid Credentials '});
+        }
+
+        let token = jwt.sign(user.toJSON(), process.env.JWT_SECRET, {
+            expiresIn: 60 * 60 * 24 * 7 // 1 week
+        });
+        
+        res.send({ token });
+    })
+    .catch(err => {
+        console.log('Error in POST /auth/login', err)
+        res.status(503).send({ message: 'Something wrong w/ DB or user input '});
+    })
+
 })
 
 router.post('/signup', (req, res) => {
